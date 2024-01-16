f69185454a1aca467907fd26a821c6c1356ae531
metod1 <metodi.ganev@gmail.com>
2024-01-16 20:20:55 +0200

    ok

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime as dt
 import random
 
 MY_EMAIL = "metodiganev207@gmail.com"
-MY_PASSWORD = "iskw rpen wajw inix"
+MY_PASSWORD = "zfse mzhi qdkr gwrc"
 today = dt.datetime.now()
 today_tuple = (today.month, today.day)
 
@@ -23,10 +23,10 @@ if today_tuple in birthday_dict.keys():
     #     connection.login(user=MY_EMAIL, password=MY_PASSWORD)
     #     connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject:Happy Birthday\n\n{new_content}")
 
-    connection = smtplib.SMTP("smtp.gmail.com")
+    connection = smtplib.SMTP("smtp.gmail.com",587)
     connection.starttls()
     connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-    connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg="ssss")
+    connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=new_content)
     connection.close()
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@ import smtplib
 MY_EMAIL = "metodiganev207@gmail.com"
 MY_PASSWORD = "vxem caov wxae ztmq"
 
-connection = smtplib.SMTP("smtp.gmail.com")
+connection = smtplib.SMTP("smtp.gmail.com",587)
 connection.starttls()
 connection.login(user=MY_EMAIL, password=MY_PASSWORD)
 connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg="ssss")
